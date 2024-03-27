5fc9e08f082f6038493d1d43bd8fff985a042e54
EvanGudmestad <eagudmestad@ranken.edu>
2024-03-27 10:23:57 -0500

    Add project files.

---
+++ RankenClassSchedule/Models/DataLayer/ClassScheduleContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RankenClassSchedule.Models.Configuration;
+using RankenClassSchedule.Models.DomainModels;
+
+namespace RankenClassSchedule.Models.DataLayer
+{
+    public class ClassScheduleContext : DbContext
+    {
+        public ClassScheduleContext(DbContextOptions<ClassScheduleContext> options)
+           : base(options)
+        { }
+
+        public DbSet<Day> Days { get; set; } = null!;
+        public DbSet<Teacher> Teachers { get; set; } = null!;
+
+        public DbSet<Class> Classes { get; set; } = null!;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Seed our Data
+            modelBuilder.ApplyConfiguration(new DayConfig());
+            modelBuilder.ApplyConfiguration(new TeacherConfig());
+            modelBuilder.ApplyConfiguration(new ClassConfig());
+        }
+
+    }
+}
---
+++ RankenClassSchedule/Models/DomainModels/Teacher.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace RankenClassSchedule.Models.DomainModels
+{
+    public class Teacher
+    {
+        public Teacher() => Classes = new HashSet<Class>(); // Constructor initializes collection
+
+        public int TeacherId { get; set; }                    // Primary key
+
+        [Display(Name = "First Name")]
+        [StringLength(100, ErrorMessage = "First name may not exceed 100 characters.")]
+        [Required(ErrorMessage = "Please enter a first name.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Display(Name = "Last Name")]
+        [StringLength(100, ErrorMessage = "Last name may not exceed 100 characters.")]
+        [Required(ErrorMessage = "Please enter a last name.")]
+        public string LastName { get; set; } = string.Empty;
+
+        // read-only display property
+        public string FullName => $"{FirstName} {LastName}";
+
+        public ICollection<Class> Classes { get; set; }       // Navigation property
+    }
+}
